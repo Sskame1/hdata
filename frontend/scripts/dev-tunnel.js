32beb17205705c1f2bef6f99129d670a993d52c5
Sskame1 <tolikstrgonov111@gmail.com>
2026-04-21 18:08:13 +0900

    dev:tunnel now starts dev server automatically

--- frontend/scripts/dev-tunnel.js
+++ frontend/scripts/dev-tunnel.js
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
+const { spawn } = require('child_process');
 const na = require('network-address');
 
 const address = na() || 'localhost';
@@ -12,3 +13,16 @@ console.log('Open these URLs on your phone (same network):');
 console.log('- Frontend: http://' + address + ':3000');
 console.log('- Backend:  http://' + address + ':3001');
 console.log('');
+
+console.log('Starting Next.js dev server...\n');
+
+const dev = spawn('npm', ['run', 'dev'], {
+  cwd: process.cwd(),
+  stdio: 'inherit',
+  shell: true,
+  env: { ...process.env, HOST: '0.0.0.0' }
+});
+
+dev.on('close', (code) => {
+  process.exit(code);
+});
