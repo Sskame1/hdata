b8f9396a14316b1195af53d89c7d24a24050b165
Sskame1 <tolikstrgonov111@gmail.com>
2026-04-21 19:38:03 +0900

    Update dev-tunnel.js

--- frontend/scripts/dev-tunnel.js
+++ frontend/scripts/dev-tunnel.js
@@ -2,13 +2,13 @@
 const localtunnel = require('localtunnel');
 
 (async () => {
-  console.log('\n=== STARTING ===\n');
+  const options = {
+    port: 3000,
+    subdomain: 'hdata'
+  };
 
-  const tunnel = await localtunnel({ port: 3000, subdomain: 'hdata' });
-
-  console.log('Open on your phone:');
-  console.log(tunnel.url);
-  console.log('');
+  const tunnel = await localtunnel(options);
+  console.log('Ваш публичный URL:', tunnel.url);
 
   tunnel.on('close', () => {
     process.exit(1);
